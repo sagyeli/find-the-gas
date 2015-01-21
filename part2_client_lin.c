1d4255f4e3d5ba338131d8ac2cfa08a923582a4b
Ubuntu <ubuntu@ip-172-31-27-123.us-west-2.compute.internal>
2015-01-21 23:11:37 +0000

    Part 2: make the client to listen toa designated port

---
+++ part2_client_lin.c
@@ -0,0 +1,213 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define PORT_NUMBER 30000
+
+#define SEA_SIZE 6
+
+int knownSea[SEA_SIZE][SEA_SIZE];
+int isMyTurn;
+
+char *replace_str(char *str, char *orig, char *rep)
+{
+	static char buffer[9096];
+	char *p;
+
+	if(!(p = (char *)strstr(str, orig)))  // Is 'orig' even in 'str'?
+		return str;
+
+	strncpy(buffer, str, p-str); // Copy characters from 'str' start to 'orig' st$
+	buffer[p-str] = '\0';
+
+	sprintf(buffer+(p-str), "%s%s", rep, p+strlen(orig));
+
+	return buffer;
+}
+
+void showKnownSea()
+{
+	int i, j;
+
+	for (i = 0 ; i < SEA_SIZE ; i++)
+	{
+		for (j = 0 ; j < SEA_SIZE ; j++)
+		{
+			printf("%d", knownSea[i][j]);
+		}
+		printf("\r\n");
+	}
+
+	isMyTurn = 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char * serverIPAddress;
+	int listenfd = 0, connfd = 0, sockfd = 0, n = 0, i, j;
+	char recvBuff[1024], sendBuff[1024], * buffer;
+	int input[2];
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in serv_addr2;
+
+	if (argc < 2)
+	{
+		printf("ERROR: no IP address was entered!\r\n");
+		return 0;
+	}
+
+	serverIPAddress = argv[1];
+
+	memset(recvBuff, '0', sizeof(recvBuff));
+	memset(sendBuff, '0', sizeof(sendBuff));
+
+	for (i = 0 ; i < SEA_SIZE ; i++)
+	{
+		for (j = 0 ; j < SEA_SIZE ; j++)
+		{
+			knownSea[i][j] = -1;
+		}
+	}
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    } 
+
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT_NUMBER); 
+
+    if(inet_pton(AF_INET, serverIPAddress, &serv_addr.sin_addr)<=0)
+    {
+        printf("\n inet_pton error occured\n");
+        return 1;
+    } 
+
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+       printf("\n Error : Connect Failed \n");
+       return 1;
+    }
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr2, '0', sizeof(serv_addr2)); 
+
+    serv_addr2.sin_family = AF_INET;
+    serv_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr2.sin_port = htons(argc > 2 ? atoi(argv[2]) : PORT_NUMBER);
+
+    bind(listenfd, (struct sockaddr*)&serv_addr2, sizeof(serv_addr2)); 
+
+    listen(listenfd, 10);
+
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    {
+	recvBuff[n] = 0;
+	buffer = replace_str(replace_str(replace_str(replace_str(replace_str(recvBuff, "'YOUR_TURN'", ""), "'NOT_YOUR_TURN'", ""), "'SPOT_TYPE_0'", ""), "'SPOT_TYPE_1'", ""), "'SPOT_TYPE_2'", "");
+	if (strstr(recvBuff, "':") != NULL)
+	{
+		buffer = replace_str(buffer, buffer, "");
+	}
+
+	if(fputs(buffer, stdout) == EOF)
+	{
+		printf("\n Error : Fputs error\n");
+	}
+
+	if (strstr(recvBuff, "'YOUR_TURN'") != NULL)
+	{
+		isMyTurn = 1;
+
+		printf("Yeah, it's my turn!\r\n");
+
+		printf("Enter row: ");
+		scanf("%d", & input[0]);
+
+		printf("Enter column: ");
+		scanf("%d", & input[1]);
+
+		snprintf(sendBuff, sizeof(sendBuff), "%d,%d\r\n", input[0], input[1]);
+		write(sockfd, sendBuff, strlen(sendBuff));
+	}
+	else if (strstr(recvBuff, "'NOT_YOUR_TURN'") != NULL)
+	{
+		isMyTurn = 0;
+
+		printf("Bummer, it's the opponent's turn...\r\n");
+	}
+	else if (strstr(recvBuff, "'SPOT_TYPE_0'") != NULL)
+	{
+		if (isMyTurn)
+		{
+			printf("I didn't find anything. What a waste of time!\r\n");
+		}
+		else
+		{
+			printf("Yes! the other guy did not find anything. Good for me!\r\n");
+		}
+
+		input[0] = atoi((const char *)strtok(replace_str(recvBuff, "'SPOT_TYPE_0':", ""), ","));
+		input[1] = atoi((const char *)strtok(NULL, ","));
+		knownSea[input[0]][input[1]] = 0;
+
+		printf("This is what I know about the sea:\r\n");
+		showKnownSea();
+	}
+	else if (strstr(recvBuff, "'SPOT_TYPE_1'") != NULL)
+	{
+		if (isMyTurn)
+		{
+			printf("I found a small gas pocket. Nice!\r\n");
+		}
+		else
+		{
+			printf("The other guy found something... :(!\r\n");
+		}
+
+		input[0] = atoi((const char *)strtok(replace_str(recvBuff, "'SPOT_TYPE_1':", ""), ","));
+		input[1] = atoi((const char *)strtok(NULL, ","));
+		knownSea[input[0]][input[1]] = 1;
+
+		printf("This is what I know about the sea:\r\n");
+		showKnownSea();
+	}
+	else if (strstr(recvBuff, "'SPOT_TYPE_2'") != NULL)
+	{
+		if (isMyTurn)
+		{
+			printf("Awsome! It looks like I hit the jackpot!!!\r\n");
+		}
+		else
+		{
+			printf("Oh no, the other guy found something big!!\r\n");
+		}
+
+		input[0] = atoi((const char *)strtok(replace_str(recvBuff, "'SPOT_TYPE_2':", ""), ","));
+		input[1] = atoi((const char *)strtok(NULL, ","));
+		knownSea[input[0]][input[1]] = 2;
+
+		printf("This is what I know about the sea:\r\n");
+		showKnownSea();
+	}
+
+	sleep(1);
+	
+    } 
+
+    if(n < 0)
+    {
+        printf("\n Read error \n");
+    } 
+
+    return 0;
+}
---
+++ part2_server_lin.c
@@ -0,0 +1,345 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#define PORT_NUMBER 30000
+
+#define SEA_SIZE 6
+ 
+struct Environment
+{
+	int sea[SEA_SIZE][SEA_SIZE];
+	int users[2];
+	char * usersIPAddresses[2];
+	int number_of_active_users;
+	int user_who_has_the_turn;
+	int current_turn_number;
+};
+
+void init(struct Environment * environment)
+{
+	environment->number_of_active_users = 0;
+	environment->current_turn_number = 0;
+}
+
+void generateSea(struct Environment * environment)
+{
+	int i, j;
+	
+	srand(time(NULL));
+	for (i = 0 ; i < SEA_SIZE ; i++)
+	{
+		for (j = 0 ; j < SEA_SIZE ; j++)
+		{
+			if (rand() % 2 && i < SEA_SIZE-1 && j < SEA_SIZE-1)
+			{
+
+				if (
+					environment->sea[i][j] != 0 && environment->sea[i][j] != 1 && 
+					environment->sea[i+1][j] != 0 && environment->sea[i+1][j] != 1 && 
+					environment->sea[i][j+1] != 0 && environment->sea[i][j+1] != 1 &&
+					environment->sea[i+1][j+1] != 0 && environment->sea[i+1][j+1] != 1 && 
+					((i == 0 && j == 0) || environment->sea[i-1][j-1] != 1) &&
+					((j == 0) || environment->sea[i][j-1] != 1) &&
+					((i == SEA_SIZE-1 && j == 0) || environment->sea[i+1][j-1] != 1) &&
+					((i == SEA_SIZE-2 && j == 0) || environment->sea[i+2][j-1] != 1) &&
+					((i == SEA_SIZE-2) || environment->sea[i+2][j] != 1) &&
+					((i == SEA_SIZE-2 && j == SEA_SIZE-1) || environment->sea[i+2][j+1] != 1) &&
+					((i == SEA_SIZE-2 && j == SEA_SIZE-2) || environment->sea[i+2][j+2] != 1) &&
+					((i == SEA_SIZE-1 && j == SEA_SIZE-2) || environment->sea[i+1][j+2] != 1) &&
+					((j == SEA_SIZE-2) || environment->sea[i][j+2] != 1) &&
+					((i == 0 && j == SEA_SIZE-2) || environment->sea[i-1][j+2] != 1) &&
+					((i == 0 && j == SEA_SIZE-1) || environment->sea[i-1][j+1] != 1) &&
+					((i == 0) || environment->sea[i-1][j] != 1)
+					)
+				{
+					environment->sea[i][j] = 1;
+					environment->sea[i][j+1] = 1;
+					environment->sea[i+1][j] = 1;
+					environment->sea[i+1][j+1] = 1;
+				}
+				else if (environment->sea[i][j] != 1)
+				{
+					environment->sea[i][j] = 0;
+				}
+			}
+			else
+			{
+				if (
+					environment->sea[i][j] != 0 && environment->sea[i][j] != 1 && 
+					((i == 0 && j == 0) || environment->sea[i-1][j-1] != 1) &&
+					((j == 0) || environment->sea[i][j-1] != 1) &&
+					((i == SEA_SIZE-1 && j == 0) || environment->sea[i+1][j-1] != 1) &&
+					((i == SEA_SIZE-1) || environment->sea[i+1][j] != 1) &&
+					((i == SEA_SIZE-1 && j == SEA_SIZE-1) || environment->sea[i+1][j+1] != 1) &&
+					((j == SEA_SIZE-1) || environment->sea[i][j+1] != 1) &&
+					((i == 0 && j == SEA_SIZE-1) || environment->sea[i-1][j+1] != 1) &&
+					((i == 0) || environment->sea[i-1][j] != 1)
+					)
+				{
+					environment->sea[i][j] = 1;
+				}
+				else if (environment->sea[i][j] != 1)
+				{
+					environment->sea[i][j] = 0;
+				}
+			}
+		}
+	}
+}
+
+void showSea(struct Environment * environment)
+{
+	int i, j;
+
+	for (i = 0 ; i < SEA_SIZE ; i++)
+	{
+		for (j = 0 ; j < SEA_SIZE ; j++)
+		{
+			printf("%d", environment->sea[i][j]);
+		}
+		printf("\r\n");
+	}
+}
+
+int getSetSpotStatus(struct Environment * environment, int userConnfd, int lat, int lng)
+{
+	if (environment->sea[lat][lng] == 0)
+	{
+		return 0;
+	}
+	else if (environment->sea[lat][lng] != 1)
+	{
+		return -1;
+	}
+	else
+	{
+		environment->sea[lat][lng] = userConnfd;
+
+		if ((lat < SEA_SIZE - 1 && environment->sea[lat+1][lng] == 1) ||
+		    (lat > 0 && environment->sea[lat-1][lng] == 1) ||
+		    (lng < SEA_SIZE - 1 && environment->sea[lat][lng+1] == 1) ||
+		    (lng > 0 && environment->sea[lat][lng-1] == 1))
+		{
+			return 2;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+}
+
+int getWinnerUserConnfd(struct Environment * environment)
+{
+	int randomUserAConnfd = -1, randomUserAHitsCount = 0,
+	    randomUserBConnfd = -1, randomUserBHitsCount = 0,
+	    totalPocketsCount = 0,
+	    i, j;
+	
+
+	for (i = 0 ; i < SEA_SIZE ; i++)
+	{
+		for (j = 0 ; j < SEA_SIZE ; j++)
+		{
+			if (environment->sea[i][j] != 0)
+			{
+				if (environment->sea[i][j] != 1)
+				{
+					if (randomUserAConnfd == -1)
+					{
+						randomUserAConnfd = environment->sea[i][j];
+					}
+					else if (randomUserBConnfd == -1)
+					{
+						randomUserBConnfd = environment->sea[i][j];
+					}
+
+					if (randomUserAConnfd == environment->sea[i][j])
+					{
+						randomUserAHitsCount++;
+					}
+					else if (randomUserBConnfd == environment->sea[i][j])
+					{
+						randomUserBHitsCount++;
+					}
+				}
+
+				totalPocketsCount++;
+			}
+		}
+	}
+
+	if (randomUserAHitsCount > totalPocketsCount / 2)
+	{
+		return randomUserAConnfd;
+	}
+
+	if (randomUserBHitsCount > totalPocketsCount / 2)
+	{
+		return randomUserBConnfd;
+	}
+
+	return 0;
+}
+
+void startListening(struct Environment * environment)
+{
+	int listenfd = 0, connfd = 0, n, i;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in client_addr;
+	socklen_t iLen = sizeof(struct sockaddr_in);
+
+	char sendBuff[1025], recvBuff[1024];
+
+	int usersInput[2], inspectionResult, winnerCheck;
+
+	srand(time(NULL));
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(sendBuff, '0', sizeof(sendBuff)); 
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(PORT_NUMBER); 
+
+	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+	listen(listenfd, 10); 
+
+	while(1)
+	{
+		if (environment->number_of_active_users < 2)
+		{
+			connfd = accept(listenfd, (struct sockaddr*)&client_addr, &iLen);			
+
+			environment->users[environment->number_of_active_users] = connfd;
+			environment->usersIPAddresses[environment->number_of_active_users] = inet_ntoa(client_addr.sin_addr);
+			environment->number_of_active_users++;
+
+			snprintf(sendBuff, sizeof(sendBuff), "Hello new user!\r\n");
+			write(connfd, sendBuff, strlen(sendBuff));
+
+			printf("A new user just logged in with the ID: %d with the IP address %s\r\n", connfd, inet_ntoa(client_addr.sin_addr));
+
+			if (environment->number_of_active_users > 2) {
+				printf("ERROR: Too many users were logged in!\r\n");
+			}
+			else if (environment->number_of_active_users == 2)
+			{
+				printf("All the users are logged in. Hooray!\r\n\r\n");
+				break;
+			}
+		}
+
+		sleep(1);
+	}
+
+	generateSea(environment);
+
+	printf("I just created a sea map and here is how it looks like:\r\n");
+	showSea(environment);
+	printf("\r\n");
+
+	environment->user_who_has_the_turn = rand() % environment->number_of_active_users;
+	printf("I also picked up the user who will have the first turn. It will be user no. %d which ahs the ID %d", environment->user_who_has_the_turn, environment->users[environment->user_who_has_the_turn]);
+	printf("\r\n");
+
+	while(1)
+	{
+		environment->current_turn_number++;
+
+		printf("It is now turn number %d and user number %d with the ID %d is playing\r\n", environment->current_turn_number, environment->user_who_has_the_turn, environment->users[environment->user_who_has_the_turn]);
+
+		for (i = 0 ; i < environment->number_of_active_users ; i++)
+		{
+			if (i == environment->user_who_has_the_turn)
+			{
+				snprintf(sendBuff, sizeof(sendBuff), "Turn number %d is your turn bro, do your thing...\r\n", environment->current_turn_number);
+				write(environment->users[i], sendBuff, strlen(sendBuff));
+
+				snprintf(sendBuff, sizeof(sendBuff), "'YOUR_TURN'");
+				write(environment->users[i], sendBuff, strlen(sendBuff));
+			}
+			else
+			{
+				snprintf(sendBuff, sizeof(sendBuff), "Turn number %d is user ID %d's turn. Waiting for him to make a move...\r\n", environment->current_turn_number, environment->users[environment->user_who_has_the_turn]);
+				write(environment->users[i], sendBuff, strlen(sendBuff));
+
+				snprintf(sendBuff, sizeof(sendBuff), "'NOT_YOUR_TURN'");
+				write(environment->users[i], sendBuff, strlen(sendBuff));
+			}
+		}
+
+		read(environment->users[environment->user_who_has_the_turn], recvBuff, sizeof(recvBuff)-1);
+
+		usersInput[0] = atoi((const char *)strtok(recvBuff, ","));
+		usersInput[1] = atoi((const char *)strtok(NULL, ","));
+
+		printf("User number %d answered: row=%d, column=%d\r\n", environment->user_who_has_the_turn, usersInput[0], usersInput[1]);
+
+		inspectionResult = getSetSpotStatus(environment, environment->users[environment->user_who_has_the_turn], usersInput[0], usersInput[1]);
+
+		winnerCheck = getWinnerUserConnfd(environment);
+		if (winnerCheck)
+		{
+			for (i = 0 ; i < environment->number_of_active_users ; i++)
+			{
+				if (environment->users[i] == winnerCheck)
+				{
+					snprintf(sendBuff, sizeof(sendBuff), "Dude, your awsome. YOU WON!!! Game WON in %d turns.\r\n", environment->current_turn_number);
+					write(environment->users[i], sendBuff, strlen(sendBuff));
+
+					printf("We have a winner! It's the user number %d with the ID %d and %s IP address!!\r\n", i, winnerCheck, environment->usersIPAddresses[i]);
+				}
+				else
+				{
+					snprintf(sendBuff, sizeof(sendBuff), "You lost man... never mind, maybe next time! Game LOST in %d turns.\r\n", environment->current_turn_number);
+					write(environment->users[i], sendBuff, strlen(sendBuff));
+				}
+
+				close(environment->users[i]);
+			}
+
+			return;
+		}
+		
+		for (i = 0 ; i < environment->number_of_active_users ; i++) {
+			snprintf(sendBuff, sizeof(sendBuff), "'SPOT_TYPE_%d':%d,%d", inspectionResult, usersInput[0], usersInput[1]);
+			write(environment->users[i], sendBuff, strlen(sendBuff));
+		}
+
+		printf("The map currently looks like this:\r\n");
+		showSea(environment);
+
+		
+
+		if (inspectionResult == 1 || inspectionResult == 2)
+		{
+		}
+		else
+		{
+			environment->user_who_has_the_turn = (environment->user_who_has_the_turn + 1) % environment->number_of_active_users;
+		}
+
+		sleep(1);
+	}
+}
+ 
+int main()
+{
+	struct Environment environment;
+
+	init(& environment);
+	startListening(& environment);
+
+	return 0;
+}
